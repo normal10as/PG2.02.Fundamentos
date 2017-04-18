d23f6da6c78f0d0c05dcde525f7743903cf5f699
Fabio Dos Santos <fabio2st@hotmail.com>
2017-04-17 22:37:51 -0300

    estructuras 2

---
+++ Fundamentos/caso.vb
@@ -0,0 +1,21 @@
+﻿Module caso
+    Sub main()
+        Dim valor As Short
+        valor = Console.ReadLine()
+        Select Case valor
+            Case 1
+                Console.WriteLine("Es uno")
+            Case 2
+                Console.WriteLine("Es dos")
+            Case 3, 5
+                Console.WriteLine("Es tres o cinco")
+            Case 6 To 8
+                Console.WriteLine("Es de seis a ocho")
+            Case is < 0
+                Console.WriteLine("Es negativo")
+            Case Else
+                Console.WriteLine("Es otro numero")
+        End Select
+        Console.ReadKey()
+    End Sub
+End Module
---
+++ Fundamentos/hacer.vb
@@ -0,0 +1,38 @@
+﻿Module hacer
+    Sub main()
+        Dim x As Short
+        x = 1
+        Do While x <= 5
+            Console.WriteLine(x)
+            x = x + 1
+        Loop
+        x = 1
+        Do Until x > 5
+            Console.WriteLine(x)
+            x = x + 1
+        Loop
+        x = 1
+        Do
+            Console.WriteLine(x)
+            x = x + 1
+        Loop While x <= 5
+        x = 1
+        Do
+            Console.WriteLine(x)
+            x = x + 1
+        Loop Until x > 5
+
+        For y As Byte = 1 To 5
+            Console.WriteLine(y)
+        Next
+        For y As Byte = 3 To 30 Step 3
+            Console.WriteLine(y)
+        Next
+        Console.ReadKey()
+
+        Do
+            Console.WriteLine("ingresar un valor entre 1 y 10")
+            x = Console.ReadLine()
+        Loop Until x >= 1 And x <= 10
+    End Sub
+End Module
