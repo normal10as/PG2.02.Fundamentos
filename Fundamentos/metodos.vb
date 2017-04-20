3cecce83a04314bb6e320ef6cbe374f27102ccbb
Fabio Dos Santos <fabio2st@hotmail.com>
2017-04-19 22:58:15 -0300

    Procedimientos

---
+++ Fundamentos/metodos.vb
@@ -0,0 +1,33 @@
+﻿Module metodos
+    Sub main()
+        holaMundo()
+        escribir("Quiero escribir esto")
+        imprimirMaximo(10, 16)
+        escribir(calcularMaximo(20, 15))
+        Console.ReadKey()
+    End Sub
+
+    Private Sub holaMundo()
+        Console.WriteLine("Hola Mundo")
+    End Sub
+
+    Private Sub escribir(cadena As String)
+        Console.WriteLine(cadena)
+    End Sub
+
+    Private Sub imprimirMaximo(valor1 As Single, valor2 As Single)
+        If valor1 > valor2 Then
+            escribir(valor1)
+        Else
+            escribir(valor2)
+        End If
+    End Sub
+
+    Private Function calcularMaximo(valor1 As Single, valor2 As Single) As Single
+        If valor1 > valor2 Then
+            Return valor1
+        Else
+            Return valor2
+        End If
+    End Function
+End Module
