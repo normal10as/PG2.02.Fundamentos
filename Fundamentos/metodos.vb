f757dc0891b12be02443e36017a23ce2ece96a9b
Fabio Dos Santos <fabio2st@hotmail.com>
2017-05-09 01:46:01 -0300

    Parámetros por referencia y valor. Procedimientos sobrecargados.

--- Fundamentos/metodos.vb
+++ Fundamentos/metodos.vb
@@ -5,8 +5,22 @@
         escribir("Quiero escribir esto")
         imprimirMaximo(10, 16)
         escribir(calcularMaximo(20, 15))
+        Dim valor As Double = 30
+        Console.WriteLine("Valor:" & valor)
+        pasoPorValor(valor)
+        Console.WriteLine("Valor otra vez:" & valor)
+        escribir(concatenar("hola", "Como te va?"))
+        escribir(concatenar("hola", "Como te va?", "todo bien?"))
         Console.ReadKey()
     End Sub
+
+    Private Function concatenar(valor1 As String, valor2 As String) As String
+        Return valor1 & " " & valor2
+    End Function
+
+    Private Function concatenar(valor1 As String, valor2 As String, valor3 As String) As String
+        Return valor1 & " " & valor2 & " " & valor3
+    End Function
 
     Private Sub holaMundo()
         Console.WriteLine("Hola Mundo")
@@ -14,6 +28,11 @@
 
     Private Sub escribir(cadena As String)
         Console.WriteLine(cadena)
+    End Sub
+
+    Private Sub pasoPorValor(ByRef dato As Single)
+        dato = dato + 10
+        Console.WriteLine("Valor mas 10:" & dato)
     End Sub
 
     Private Sub imprimirMaximo(valor1 As Single, valor2 As Single)
