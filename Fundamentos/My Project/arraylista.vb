448371c971545e52d254eb695241a360a20ac40a
Fabio Dos Santos <fabio2st@hotmail.com>
2017-05-23 19:27:32 -0300

    collection y arraylist

---
+++ Fundamentos/My Project/arraylista.vb
@@ -0,0 +1,36 @@
+﻿Module arraylista
+    Sub main()
+        ' crea la coleccion
+        Dim frutas As ArrayList
+        ' inicializa la colección
+        frutas = New ArrayList
+        ' agregar elementos
+        frutas.Add("Manzanas")
+        frutas.Add("Peras")
+        frutas.Add("Frutillas")
+        frutas.Add("Tomate")
+        Console.WriteLine("CAntidad: " & frutas.Count)
+        ' iterar
+        frutas.RemoveAt(3)
+        frutas.Remove("Peras")
+        frutas.Insert(0, "Mandarina")
+        frutas.Insert(2, "Naranja")
+        For Each fruta In frutas
+            Console.WriteLine(fruta)
+        Next
+        frutas.Sort()
+        frutas.Reverse()
+
+        For Each fruta In frutas
+            Console.WriteLine(fruta)
+        Next
+
+        Console.WriteLine("segundo: " & frutas.Item(1))
+        frutas.Clear()
+        Console.WriteLine("borrado todo")
+        For Each fruta In frutas
+            Console.WriteLine(fruta)
+        Next
+    End Sub
+
+End Module
--- Fundamentos/coleccion.vb
+++ Fundamentos/coleccion.vb
@@ -5,12 +5,25 @@
         ' inicializa la colección
         frutas = New Collection
         ' agregar elementos
-        frutas.Add("Manzanas")
-        frutas.Add("Peras")
-        frutas.Add("Frutillas")
+        frutas.Add("Manzanas", "mz")
+        frutas.Add("Peras", "pe")
+        frutas.Add("Frutillas", "fr")
+        frutas.Add("Tomate","to")
+        Console.WriteLine("CAntidad: " & frutas.Count)
         ' iterar
+        frutas.Remove(4)
         For Each fruta In frutas
             Console.WriteLine(fruta)
         Next
+
+        Console.WriteLine("segundo: " & frutas.Item(2))
+        Console.WriteLine("segundo: " & frutas.Item("fr"))
+        frutas.Clear()
+        Console.WriteLine("borrado todo")
+        For Each fruta In frutas
+            Console.WriteLine(fruta)
+        Next
+
+
     End Sub
 End Module
